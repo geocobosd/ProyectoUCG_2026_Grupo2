77a1a733d8a02bf405e78bbb3fc46dca5c488a45
Geo Cobos <geo.cobosd@gmail.com>
2026-05-20 16:55:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.title("Proyecto final UCG")
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
