d7c8d232e85495cdded1358da2a337744ea6d093
Geo Cobos <geo.cobosd@gmail.com>
2026-05-20 16:58:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import libreria_funciones as lf
 
+st.sidebar.title("configuración")
 st.title("Proyecto final UCG")
 
 uploaded_files = st.file_uploader(
